c3ca5ac3ba93bb9f76e0ccb26834b6607e4ed187
root <root@OpenWrt.lan>
2021-02-04 17:12:22 +0800

    update gfw list

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 ./gfwlist2dnsmasq.sh -d 8.8.8.8 -p 53 -s gfwlist -o gfwlist.conf --exclude-domain-file exclude.domain --extra-domain-file extra.domain
 cp gfwlist.conf  /tmp/dnsmasq.d/
+cp gfwlist.conf  /etc/dnsmasq.d/
 /etc/init.d/dnsmasq restart
