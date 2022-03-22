036674e1132cea86b2b8baee89b68975f5bbeb69
rengame <ipart.ivan@gmail.com>
2022-03-22 10:00:20 +0800

    增加smartdns支持

--- run.sh
+++ run.sh
@@ -1,4 +1,9 @@
 ./gfwlist2dnsmasq.sh -d 8.8.8.8 -p 53 -s gfwlist -o gfwlist.conf --exclude-domain-file exclude.domain --extra-domain-file extra.domain
+./gfwlist2smartdns.sh -g vpn -s gfwlist -o ./test.conf -t smartdns --exclude-domain-file exclude.domain --extra-domain-file extra.domain -o gfwlistforsmartdns.conf
+
 cp gfwlist.conf  /tmp/dnsmasq.d/
 cp gfwlist.conf  /etc/dnsmasq.d/
 /etc/init.d/dnsmasq restart
+
+cp gfwlistforsmartdns.conf /etc/smartdns/
+/etc/init.d/smartdns restart
