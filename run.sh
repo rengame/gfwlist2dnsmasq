b2379b88720ec8063d24e9a8f43d9026a80bbef6
rengame <ipart.ivan@gmail.com>
2022-03-22 11:04:19 +0800

    add wiki.factorio.com domain

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 ./gfwlist2dnsmasq.sh -d 8.8.8.8 -p 53 -s gfwlist -o gfwlist.conf --exclude-domain-file exclude.domain --extra-domain-file extra.domain
-./gfwlist2smartdns.sh -g vpn -s gfwlist -o ./test.conf -t smartdns --exclude-domain-file exclude.domain --extra-domain-file extra.domain -o gfwlistforsmartdns.conf
+./gfwlist2dnsmasq.sh -g vpn -s gfwlist -o ./test.conf -t smartdns --exclude-domain-file exclude.domain --extra-domain-file extra.domain -o gfwlistforsmartdns.conf
 
 cp gfwlist.conf  /tmp/dnsmasq.d/
 cp gfwlist.conf  /etc/dnsmasq.d/
