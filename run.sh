0648a7ef23c04036e0c143e1b951a4f2ccdb5f8e
rengame <ipart.ivan@gmail.com>
2025-02-02 22:43:32 +0800

    update gfw domain list

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
-./gfwlist2dnsmasq.sh -d 8.8.8.8 -p 53 -s gfwlist -o gfwlist.conf --exclude-domain-file exclude.domain --extra-domain-file extra.domain
+./gfwlist2dnsmasq.sh -d 1.1.1.1 -p 53 -s gfwlist -o gfwlist.conf --exclude-domain-file exclude.domain --extra-domain-file extra.domain
 ./gfwlist2dnsmasq.sh --group vpn --ipset gfwlist --out-type smartdns --exclude-domain-file exclude.domain --extra-domain-file extra.domain --output gfwlistforsmartdns.conf
 
 cp gfwlist.conf  /tmp/dnsmasq.d/
 cp gfwlist.conf  /etc/dnsmasq.d/
 /etc/init.d/dnsmasq restart
-/etc/init.d/adguardhome restart
+# /etc/init.d/adguardhome restart
 
-cp gfwlistforsmartdns.conf /etc/smartdns/
-/etc/init.d/smartdns restart
+# cp gfwlistforsmartdns.conf /etc/smartdns/
+# /etc/init.d/smartdns restart
